01a7219f83b644341d6939fe5bb2a197d04cb119
Bharat Mali <158173770+bharat1704@users.noreply.github.com>
2025-01-02 23:39:28 +0530

    Update App.jsx
    Updated app.jsx

--- rollUpState/src/App.jsx
+++ rollUpState/src/App.jsx
@@ -1,7 +1,38 @@
-export default function App(){
+import { useState } from 'react'
+import './App.css'
 
+function App() {
   return <div>
-
-    Hi there
+    <LightBulb />
   </div>
 }
+
+function LightBulb() {
+  const [bulbOn, setBulbOn] = useState(true)
+
+  return <div>
+    <BulbState bulbOn={bulbOn} />
+    <ToggleBulbState bulbOn={bulbOn} setBulbOn={setBulbOn} />
+  </div>
+}
+
+function BulbState({bulbOn}) {
+  return <div>
+    {bulbOn ? "Bulb on" : "Bulb off"}
+  </div>
+}
+
+function ToggleBulbState({bulbOn, setBulbOn}) {
+
+  function toggle() {
+    // setBulbOn(currentState => !currentState)
+    setBulbOn(!bulbOn)
+    
+  }
+
+  return <div>
+    <button onClick={toggle}>Toggle the bulb</button>
+  </div>
+}
+
+export default App
