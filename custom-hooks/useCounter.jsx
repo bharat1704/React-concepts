d59404ca3c6e757bd761a14b28320ae7781f307d
Bharat Mali <158173770+bharat1704@users.noreply.github.com>
2025-02-25 07:33:14 +0530

    Update useCounter.jsx

--- custom-hooks/useCounter.jsx
+++ custom-hooks/useCounter.jsx
@@ -1,26 +1,36 @@
-import { useState } from "react"
+  import { useState } from "react"
 
-//custom hook
-function useCounter(){
-  const[count, SetCount] = useState(0);
+  //custom hook
+  function useCounter(){
+    const[count, SetCount] = useState(0);
 
-  function increasecount(){
-    SetCount(count+1);
+    function increasecount(){
+      SetCount(count+1);
+    }
+    return{
+      count:count,
+      increasecount:increasecount
+    }
   }
-  return{
-    count:count,
-    increasecount:increasecount
+
+  export default function App(){
+
+  return <div> 
+    <Counter> </Counter>
+    <Counter> </Counter>
+    <Counter> </Counter>
+  </div>
+
   }
-}
 
-export default function App(){
-  const [count,increasecount] = useCounter;
-  
-return <div>
+  function Counter(){
 
-  <button onClick={increasecount}> Counter </button>
-{count}
-</div>
+    const{count,increasecount} = useCounter();
 
 
-}
+  return <div>
+    <button onClick={increasecount}> Counter {count} </button>
+
+  </div>
+
+  }
